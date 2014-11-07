971f687fa6a1a525fd4ce5ab079eee51a0ca0207
Tharman Anantharajan <tharman@gmail.com>
2014-11-07 11:08:10 +0000

    Re-organising strings for use in SQL queries

--- app/src/main/java/io/github/akz08/cyoaclient/data/DatabaseContract.java
+++ app/src/main/java/io/github/akz08/cyoaclient/data/DatabaseContract.java
@@ -11,12 +11,21 @@ public class DatabaseContract {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "cyoa.db";
 
-    private static final String INTEGER_TYPE = " INTEGER";
-    private static final String TEXT_TYPE = " TEXT";
-    private static final String DATETIME_TYPE = " DATETIME";
-    private static final String AUTO_DATETIME = " DEFAULT CURRENT_TIMESTAMP";
-    private static final String BLOB_TYPE = " BLOB";
-    private static final String COMMA_SEP = ",";
+    // String constants used repeatedly in SQL query construction.
+    private static final String COMMA_SEP = " , ";
+    private static final String L_PAREN = " ( ";
+    private static final String R_PAREN = " ) ";
+    private static final String SEMI_COLON = " ; ";
+
+    private static final String INTEGER_TYPE = " INTEGER ";
+    private static final String TEXT_TYPE = " TEXT ";
+    private static final String DATETIME_TYPE = " DATETIME ";
+    private static final String BLOB_TYPE = " BLOB ";
+
+    private static final String INTEGER_PRIMARY_KEY = " INTEGER PRIMARY KEY ";
+    private static final String FOREIGN_KEY = " FOREIGN KEY ";
+    private static final String REFERENCES = " REFERENCES ";
+    private static final String AUTO_DATETIME = " DEFAULT CURRENT_TIMESTAMP ";
 
     // To prevent accidental instantiation of the contract class, give it an empty constructor.
     private DatabaseContract() {}
@@ -35,13 +44,13 @@ public class DatabaseContract {
 
         // Create table method
         public static final String CREATE_TABLE = "CREATE TABLE " +
-            TABLE_NAME + " (" +
-                _ID + " INTEGER PRIMARY KEY," +
+            TABLE_NAME + L_PAREN +
+                _ID + INTEGER_PRIMARY_KEY + COMMA_SEP +
                 COLUMN_NAME + TEXT_TYPE + COMMA_SEP +
                 COLUMN_AGE + TEXT_TYPE + COMMA_SEP +
                 COLUMN_DESCRIPTION + TEXT_TYPE + COMMA_SEP +
                 COLUMN_PHOTO + BLOB_TYPE +
-            " )";
+            R_PAREN + SEMI_COLON;
 
         // Delete table method
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
@@ -60,13 +69,13 @@ public class DatabaseContract {
 
         // Create table method
         public static final String CREATE_TABLE = "CREATE TABLE " +
-            TABLE_NAME + " (" +
-                _ID + " INTEGER PRIMARY KEY," +
+            TABLE_NAME + L_PAREN +
+                _ID + INTEGER_PRIMARY_KEY + COMMA_SEP +
                 COLUMN_CHARACTER_ID + INTEGER_TYPE + COMMA_SEP +
                 COLUMN_SCENE_INFORMATION + TEXT_TYPE + COMMA_SEP +
-                " FOREIGN KEY (" + COLUMN_CHARACTER_ID + ") REFERENCES " +
-                CharacterEntry.TABLE_NAME + " (" + CharacterEntry._ID + "), " +
-            ");";
+                FOREIGN_KEY + L_PAREN + COLUMN_CHARACTER_ID + R_PAREN + REFERENCES +
+                    CharacterEntry.TABLE_NAME + L_PAREN + CharacterEntry._ID + R_PAREN +
+            R_PAREN + SEMI_COLON;
 
         // Delete table method
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
@@ -86,14 +95,14 @@ public class DatabaseContract {
 
         // Create table method
         public static final String CREATE_TABLE = "CREATE TABLE " +
-            TABLE_NAME + " (" +
-                _ID + " INTEGER PRIMARY KEY," +
+            TABLE_NAME + L_PAREN +
+                _ID + INTEGER_PRIMARY_KEY + COMMA_SEP +
                 COLUMN_SCENE_ID + INTEGER_TYPE + COMMA_SEP +
                 COLUMN_MESSAGE_TEXT + TEXT_TYPE + COMMA_SEP +
                 COLUMN_MESSAGE_DATETIME + DATETIME_TYPE + AUTO_DATETIME + COMMA_SEP +
-                " FOREIGN KEY (" + COLUMN_SCENE_ID + ") REFERENCES " +
-                CharacterEntry.TABLE_NAME + " (" + CharacterEntry._ID + "), " +
-            ");";
+                FOREIGN_KEY + L_PAREN + COLUMN_SCENE_ID + R_PAREN + REFERENCES +
+                    CharacterEntry.TABLE_NAME + L_PAREN + CharacterEntry._ID + R_PAREN +
+            R_PAREN + SEMI_COLON;
 
         // Delete table method
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
@@ -114,15 +123,15 @@ public class DatabaseContract {
 
         // Create table method
         public static final String CREATE_TABLE = "CREATE TABLE " +
-            TABLE_NAME + " (" +
-                _ID + " INTEGER PRIMARY KEY," +
+            TABLE_NAME + L_PAREN +
+                _ID + INTEGER_PRIMARY_KEY + COMMA_SEP +
                 COLUMN_MESSAGE_ID + INTEGER_TYPE + COMMA_SEP +
                 COLUMN_CHOICE_TEXT + TEXT_TYPE + COMMA_SEP +
                 COLUMN_CHOICE_DATETIME + DATETIME_TYPE + AUTO_DATETIME + COMMA_SEP +
                 COLUMN_CHOICE_SELECTED + INTEGER_TYPE + COMMA_SEP +
-                " FOREIGN KEY (" + COLUMN_MESSAGE_ID + ") REFERENCES " +
-                MessageEntry.TABLE_NAME + " (" + MessageEntry._ID + "), " +
-            ");";
+                FOREIGN_KEY + L_PAREN + COLUMN_MESSAGE_ID + R_PAREN + REFERENCES +
+                    MessageEntry.TABLE_NAME + L_PAREN + MessageEntry._ID + R_PAREN +
+            R_PAREN + SEMI_COLON;
 
         // Delete table method
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
