724ea39f2ccf52c7a79a9d4fdf3466b9a80e7a98
Tharman Anantharajan <tharman@gmail.com>
2014-11-09 20:32:53 +0000

    Completing basic JUnit testing on database
    After the creation of tables, mock data is inserted
    and retrieved. The retrieved query values are then
    compared with the original mock data to ensure that
    values and data type are preserved.
    
    Additionally, scene, message and choice records cannot
    be inserted without an existing record in their respective
    dependent tables, so referential integrity is also tested
    at a basic level.
    
    A slight change is also made to the choice table schema
    to reduce a redundancy in timestamping. Only the date of
    choice selection must be recorded by the client. This field
    is created with a null value initially.

--- app/src/androidTest/java/io/github/akz08/cyoaclient/TestDatabase.java
+++ app/src/androidTest/java/io/github/akz08/cyoaclient/TestDatabase.java
@@ -1,20 +1,151 @@
 package io.github.akz08.cyoaclient;
 
+import android.annotation.TargetApi;
+import android.content.ContentValues;
+import android.content.res.Resources;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Set;
 
 import io.github.akz08.cyoaclient.data.DatabaseContract;
 import io.github.akz08.cyoaclient.data.DatabaseHelper;
 
+@TargetApi(11)
 public class TestDatabase extends AndroidTestCase {
 
-    public static final String LOG_CAT = TestDatabase.class.getSimpleName();
+    private Resources res;
+    private DatabaseHelper databaseHelper;
+    private SQLiteDatabase database;
 
-    public void testCreateDatabase() throws Throwable {
-        mContext.deleteDatabase(DatabaseContract.DATABASE_NAME);
-        SQLiteDatabase database = new DatabaseHelper(this.mContext).getWritableDatabase();
+    public void setUp() throws Exception {
+        super.setUp();
+        // Assign resources from the project for testing purposes
+        res = getContext().getResources();
+        // Provide a different context name, so that the device database is left intact
+        RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
+        databaseHelper = new DatabaseHelper(context);
+        database = databaseHelper.getWritableDatabase();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+        databaseHelper.close();
+    }
+
+    public void testDatabaseIsOpen() {
         assertEquals(true, database.isOpen());
-        database.close();
+    }
+
+    public void testInsertReadTables() {
+        // Generate character mock data
+        ContentValues characterValues = createCharacterValues();
+        long characterRowId = database.insert(DatabaseContract.CharacterEntry.TABLE_NAME, null, characterValues);
+        assertTrue(characterRowId != -1);
+        Cursor characterTable = database.query(DatabaseContract.CharacterEntry.TABLE_NAME, null, null, null, null, null, null);
+        validateCursor(characterTable, characterValues);
+        // Generate scene mock data
+        ContentValues sceneValues = createSceneValues((int) characterRowId);
+        long sceneRowId = database.insert(DatabaseContract.SceneEntry.TABLE_NAME, null, sceneValues);
+        assertTrue(sceneRowId != -1);
+        Cursor sceneTable = database.query(DatabaseContract.SceneEntry.TABLE_NAME, null, null, null, null, null, null);
+        validateCursor(sceneTable, sceneValues);
+        // Generate message mock data
+        ContentValues messageValues = createMessageValues((int) sceneRowId);
+        long messageRowId = database.insert(DatabaseContract.MessageEntry.TABLE_NAME, null, messageValues);
+        assertTrue(messageRowId != -1);
+        Cursor messageTable = database.query(DatabaseContract.MessageEntry.TABLE_NAME, null, null, null, null, null, null);
+        validateCursor(messageTable, messageValues);
+        // Generate choice mock data
+        ContentValues choiceValues = createChoiceValues((int) messageRowId);
+        long choiceRowId = database.insert(DatabaseContract.ChoiceEntry.TABLE_NAME, null, choiceValues);
+        assertTrue(choiceRowId != -1);
+        Cursor choiceTable = database.query(DatabaseContract.ChoiceEntry.TABLE_NAME, null, null, null, null, null, null);
+        validateCursor(choiceTable, choiceValues);
+    }
+
+    private void validateCursor(Cursor valueCursor, ContentValues expectedValues) {
+        assertTrue(valueCursor.moveToFirst());
+        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
+        for (Map.Entry<String, Object> entry : valueSet) {
+            String columnName = entry.getKey();
+            int idx = valueCursor.getColumnIndex(columnName);
+            assertFalse(idx == -1);
+            int columnType = valueCursor.getType(idx);
+            if (columnType == Cursor.FIELD_TYPE_BLOB) {
+                byte[] expectedValue = (byte[]) entry.getValue();
+                assertEquals(Arrays.toString(expectedValue), Arrays.toString(valueCursor.getBlob(idx)));
+            }
+            else if (columnType == Cursor.FIELD_TYPE_FLOAT) {
+                float expectedValue = (Float) entry.getValue();
+                assertEquals(expectedValue, valueCursor.getFloat(idx));
+            }
+            else if (columnType == Cursor.FIELD_TYPE_INTEGER) {
+                int expectedValue = (Integer) entry.getValue();
+                assertEquals(expectedValue, valueCursor.getInt(idx));
+            }
+            else if (columnType == Cursor.FIELD_TYPE_NULL) {
+                Object expectedValue = entry.getValue();
+                assertNull(expectedValue);
+            }
+            else if (columnType == Cursor.FIELD_TYPE_STRING) {
+                String expectedValue = entry.getValue().toString();
+                assertEquals(expectedValue, valueCursor.getString(idx));
+            }
+        }
+        valueCursor.close();
+    }
+
+    // Helper methods for generating mock data.
+
+    private ContentValues createCharacterValues() {
+        ContentValues characterValues = new ContentValues();
+        characterValues.put(DatabaseContract.CharacterEntry.COLUMN_NAME, "Claire");
+        characterValues.put(DatabaseContract.CharacterEntry.COLUMN_AGE, 28);
+        characterValues.put(DatabaseContract.CharacterEntry.COLUMN_DESCRIPTION, "Has just met someone.");
+        Bitmap image = BitmapFactory.decodeResource(res, R.drawable.ic_launcher);
+        byte[] buffer = blobifyImage(image);
+        characterValues.put(DatabaseContract.CharacterEntry.COLUMN_PHOTO, buffer);
+        return characterValues;
+    }
+
+    private ContentValues createSceneValues(int characterRowId) {
+        ContentValues sceneValues = new ContentValues();
+        sceneValues.put(DatabaseContract.SceneEntry.COLUMN_CHARACTER_ID, characterRowId);
+        sceneValues.put(DatabaseContract.SceneEntry.COLUMN_SCENE_INFORMATION, "Claire had a date last night. She is contacting you to ask for advice.");
+        return sceneValues;
+    }
+
+    private ContentValues createMessageValues(int sceneRowId) {
+        ContentValues messageValues = new ContentValues();
+        messageValues.put(DatabaseContract.MessageEntry.COLUMN_SCENE_ID, sceneRowId);
+        messageValues.put(DatabaseContract.MessageEntry.COLUMN_MESSAGE_TEXT, "Hi. I just met someone last night. Should I see him again?");
+        messageValues.put(DatabaseContract.MessageEntry.COLUMN_MESSAGE_DATETIME, Calendar.getInstance().get(Calendar.SECOND));
+        return messageValues;
+    }
+
+    private ContentValues createChoiceValues(int messageRowId) {
+        ContentValues choiceValues = new ContentValues();
+        choiceValues.put(DatabaseContract.ChoiceEntry.COLUMN_MESSAGE_ID, messageRowId);
+        choiceValues.put(DatabaseContract.ChoiceEntry.COLUMN_CHOICE_TEXT, "Sure.");
+        choiceValues.putNull(DatabaseContract.ChoiceEntry.COLUMN_CHOICE_SELECTED_DATETIME);
+        return choiceValues;
+    }
+
+    // Helper methods for data processing.
+
+    private byte[] blobifyImage(Bitmap image) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.PNG, 100, out);
+        return out.toByteArray();
     }
 
 }
--- app/src/main/java/io/github/akz08/cyoaclient/data/DatabaseContract.java
+++ app/src/main/java/io/github/akz08/cyoaclient/data/DatabaseContract.java
@@ -19,13 +19,11 @@ public class DatabaseContract {
 
     private static final String INTEGER_TYPE = " INTEGER ";
     private static final String TEXT_TYPE = " TEXT ";
-    private static final String DATETIME_TYPE = " DATETIME ";
     private static final String BLOB_TYPE = " BLOB ";
 
     private static final String INTEGER_PRIMARY_KEY = " INTEGER PRIMARY KEY ";
     private static final String FOREIGN_KEY = " FOREIGN KEY ";
     private static final String REFERENCES = " REFERENCES ";
-    private static final String AUTO_DATETIME = " DEFAULT CURRENT_TIMESTAMP ";
 
     // To prevent accidental instantiation of the contract class, give it an empty constructor.
     private DatabaseContract() {}
@@ -37,10 +35,10 @@ public class DatabaseContract {
 
         // Table fields
         public static final String TABLE_NAME = "character";
-        public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_AGE = "age";
-        public static final String COLUMN_DESCRIPTION = "description";
-        public static final String COLUMN_PHOTO = "photo";
+        public static final String COLUMN_NAME = "character_name";
+        public static final String COLUMN_AGE = "character_age";
+        public static final String COLUMN_DESCRIPTION = "character_description";
+        public static final String COLUMN_PHOTO = "character_photo";
 
         // Create table method
         public static final String CREATE_TABLE = "CREATE TABLE " +
@@ -99,7 +97,7 @@ public class DatabaseContract {
                 _ID + INTEGER_PRIMARY_KEY + COMMA_SEP +
                 COLUMN_SCENE_ID + INTEGER_TYPE + COMMA_SEP +
                 COLUMN_MESSAGE_TEXT + TEXT_TYPE + COMMA_SEP +
-                COLUMN_MESSAGE_DATETIME + DATETIME_TYPE + AUTO_DATETIME + COMMA_SEP +
+                COLUMN_MESSAGE_DATETIME + TEXT_TYPE + COMMA_SEP +
                 FOREIGN_KEY + L_PAREN + COLUMN_SCENE_ID + R_PAREN + REFERENCES +
                     CharacterEntry.TABLE_NAME + L_PAREN + CharacterEntry._ID + R_PAREN +
             R_PAREN + SEMI_COLON;
@@ -118,8 +116,7 @@ public class DatabaseContract {
         public static final String TABLE_NAME = "choice";
         public static final String COLUMN_MESSAGE_ID = "message_id";
         public static final String COLUMN_CHOICE_TEXT = "choice_text";
-        public static final String COLUMN_CHOICE_DATETIME = "choice_datetime";
-        public static final String COLUMN_CHOICE_SELECTED = "choice_selected";
+        public static final String COLUMN_CHOICE_SELECTED_DATETIME = "choice_selected_datetime";
 
         // Create table method
         public static final String CREATE_TABLE = "CREATE TABLE " +
@@ -127,8 +124,7 @@ public class DatabaseContract {
                 _ID + INTEGER_PRIMARY_KEY + COMMA_SEP +
                 COLUMN_MESSAGE_ID + INTEGER_TYPE + COMMA_SEP +
                 COLUMN_CHOICE_TEXT + TEXT_TYPE + COMMA_SEP +
-                COLUMN_CHOICE_DATETIME + DATETIME_TYPE + AUTO_DATETIME + COMMA_SEP +
-                COLUMN_CHOICE_SELECTED + INTEGER_TYPE + COMMA_SEP +
+                COLUMN_CHOICE_SELECTED_DATETIME + TEXT_TYPE + COMMA_SEP +
                 FOREIGN_KEY + L_PAREN + COLUMN_MESSAGE_ID + R_PAREN + REFERENCES +
                     MessageEntry.TABLE_NAME + L_PAREN + MessageEntry._ID + R_PAREN +
             R_PAREN + SEMI_COLON;
