145b982406669b38ecd828792225ae11ba7d8ad6
Amer Kamil Zainal <amer.kamil@gmail.com>
2014-11-09 17:45:43 +0000

    Retrieve uid and token from logged in session

--- app/src/main/java/io/github/akz08/cyoaclient/MainActivity.java
+++ app/src/main/java/io/github/akz08/cyoaclient/MainActivity.java
@@ -1,14 +1,22 @@
 package io.github.akz08.cyoaclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
 
 import java.util.Arrays;
@@ -59,13 +67,106 @@ public class MainActivity extends ActionBarActivity {
         public MainFragment() {
         }
 
+        private static final String TAG = "MainFragment";
+//        private GraphUser user;
+
+        private void makeMeRequest(final Session session) {
+            Request request = Request.newMeRequest(session,
+                    new Request.GraphUserCallback() {
+                        @Override
+                        public void onCompleted(GraphUser user, Response response) {
+                            // If the response is successful
+                            if (session == Session.getActiveSession()) {
+                                if (user != null) {
+                                    // get the access token and uid
+                                    // this stuff should be sent to the app API to register for
+                                    //  a new user
+                                    Log.i(TAG, session.getAccessToken());
+                                    Log.i(TAG, user.getId());
+
+                                    Log.i(TAG, user.getName());
+                                }
+                            }
+                            if (response.getError() != null) {
+                                // Handle errors, will do so later.
+                            }
+                        }
+                    });
+            request.executeAsync();
+        }
+
+
+        private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+            if (state.isOpened()) {
+                Log.i(TAG, "Logged in...");
+            } else if (state.isClosed()) {
+                Log.i(TAG, "Logged out...");
+            }
+
+            if (session != null && session.isOpened()) {
+                makeMeRequest(session);
+            }
+        }
+
+        private Session.StatusCallback callback = new Session.StatusCallback() {
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                onSessionStateChange(session, state, exception);
+            }
+        };
+
+        private UiLifecycleHelper uiHelper;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            uiHelper = new UiLifecycleHelper(getActivity(), callback);
+            uiHelper.onCreate(savedInstanceState);
+        }
+
+        // Override fragment lifecycle methods to get UiLifecycleHelper working correctly
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            uiHelper.onResume();
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            uiHelper.onActivityResult(requestCode, resultCode, data);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            uiHelper.onPause();
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            uiHelper.onDestroy();
+        }
+
+        @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            uiHelper.onSaveInstanceState(outState);
+        }
+
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
             LoginButton authButton = (LoginButton) rootView.findViewById(R.id.authButton);
+
+            // this allows fragment to receive onActivityResult()
             authButton.setFragment(this);
+
             authButton.setReadPermissions(Arrays.asList("email", "public_profile"));
             return rootView;
         }
