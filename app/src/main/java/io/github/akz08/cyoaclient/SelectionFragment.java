cb4122ba4b04505e8e5b960e2c2484225cff3ac2
Tharman Anantharajan <tharman@gmail.com>
2014-11-16 22:47:21 +0000

    Adding transition to setup activity
    Fixed transition issue caused by the use of a viewless
    fragment when in authenticated state. Currently login
    activity uses two fragments, but one of them inflates
    a blank view (could be wasteful?).

--- app/src/main/java/io/github/akz08/cyoaclient/SelectionFragment.java
+++ app/src/main/java/io/github/akz08/cyoaclient/SelectionFragment.java
@@ -1,6 +1,10 @@
 package io.github.akz08.cyoaclient;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +15,29 @@ public class SelectionFragment extends Fragment {
     public SelectionFragment() {}
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_selection, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Access shared preferences to check whether this is the first app launch
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        Boolean is_first_launch = prefs.getBoolean("is_first_launch", true);
+        if (is_first_launch) {
+            //prefs.edit().putBoolean("is_first_time", false).commit();
+            Intent intent = new Intent(getActivity(), SetupActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            getActivity().finish();
+        }
+        else {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            getActivity().finish();
+        }
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
 }
---
+++ app/src/main/java/io/github/akz08/cyoaclient/SetupActivity.java
@@ -0,0 +1,14 @@
+package io.github.akz08.cyoaclient;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class SetupActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+    }
+
+}
